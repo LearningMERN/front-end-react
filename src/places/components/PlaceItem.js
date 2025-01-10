5f2af4388a6e07274bf91a4db8647b9f51fcdfde
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2025-01-10 14:14:18 -0600

    began work on modal component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../../shared/components/UIElements/Card";
 import './PlaceItem.css'
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 
 //static component for stylizing of a single element
 //props are passed through PlaceList
 const PlaceItem = props => {
+    const [showMap, setShowMap] = useState(false);
+
+    const openMapHandler = () => {
+        setShowMap(true);
+    }
+
+    const closeMapHandler = () => {
+        setShowMap(false);
+    }
+
+
     return (
+        <React.Fragment>
+            <Modal show={showMap}
+                   onCancel={closeMapHandler}
+                   header={props.address} contentClass="place-item__modal-content" footerClass="place-item__modal-actions"
+                   footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+            >
+            <div className="map-container">
+                <h2>THE MAP</h2>
+            </div>
+            </Modal>
         <li className="place-item">
             <Card className="place-item__content">
                 <div className="place-item__image">
@@ -17,12 +40,13 @@ const PlaceItem = props => {
                     <p>{props.description}</p>
                 </div>
                 <div className="place-item__actions">
-                    <button>VIEW ON MAP</button>
-                    <button>EDIT</button>
-                    <button>DELETE</button>
+                    <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+                    <Button to={`/places/${props.id}`}>EDIT</Button>
+                    <Button danger >DELETE</Button>
                 </div>
             </Card>
         </li>
+        </React.Fragment>
     )
 }
 
